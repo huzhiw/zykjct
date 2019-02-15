bb461909f63b10a204949fc068d65db7028508ca
huzhiwen <1077483@qq.com>
2019-02-15 14:12:51 +0800

    修改配置

--- kernel/src/main/java/com/zykjct/kernel/core/exception/DefaultExceptionHandler.java
+++ kernel/src/main/java/com/zykjct/kernel/core/exception/DefaultExceptionHandler.java
@@ -4,12 +4,8 @@ import com.zykjct.kernel.core.exception.enums.CoreExceptionEnum;
 import com.zykjct.kernel.core.reqres.response.ErrorResponseData;
 import com.zykjct.kernel.core.reqres.response.ResponseData;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @description:  全局的的异常拦截器（拦截所有的控制器）（带有@RequestMapping注解的方法上都会拦截）
@@ -17,16 +13,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @create: 2019-01-26 17:59
  **/
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
-@Order(200)
 public class DefaultExceptionHandler {
     /**
      * 拦截各个服务的具体异常
      */
     @ExceptionHandler(ApiServiceException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
     public ResponseData apiService(ApiServiceException e) {
         log.error("服务具体异常:", e);
         ErrorResponseData errorResponseData = new ErrorResponseData(e.getCode(), e.getErrorMessage());
@@ -39,7 +33,6 @@ public class DefaultExceptionHandler {
      */
     @ExceptionHandler(ServiceException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
     public ResponseData notFount(ServiceException e) {
         log.info("业务异常:", e);
         return new ErrorResponseData(e.getCode(), e.getErrorMessage());
@@ -50,7 +43,6 @@ public class DefaultExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
     public ResponseData serverError(Exception e) {
         log.error("运行时异常:", e);
         return new ErrorResponseData(CoreExceptionEnum.SERVICE_ERROR.getCode(), CoreExceptionEnum.SERVICE_ERROR.getMessage());
--- user/src/main/java/com/zykjct/user/UserApplication.java
+++ user/src/main/java/com/zykjct/user/UserApplication.java
@@ -2,15 +2,16 @@ package com.zykjct.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringCloudApplication
 @EnableFeignClients
 @EnableHystrixDashboard
+@SpringBootApplication(scanBasePackages = {"com.zykjct.kernel","com.zykjct.user"})
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class UserApplication {
 
     public static void main(String[] args) {
